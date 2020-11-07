da48c05a2969178c8059a34c154faf4903477f7e
GraceUmutesi <tesigrace98@gmail.com>
2020-11-07 09:38:12 +0200

    all new changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { GoalComponent } from './goal/goal.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'goals', component: GoalComponent},
+  { path: 'about', component: AboutComponent},
+  { path:'**', component:NotFoundComponent},
+
+  { path: '', redirectTo:"/goals", pathMatch:"full"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
